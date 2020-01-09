5b35637b1010c1d0cfc6219fbb2b9dd1e9d37e5f
Dorian Maliszewski <maliszewskid3@gmail.com>
2020-01-10 00:16:30 +0100

    [WIP] Cleaning Group By Gender, Target,...

--- 5RBIG.r
+++ 5RBIG.r
@@ -8,6 +8,12 @@ if (!require(ggplot2)) {
 library(tidyverse)
 library(readr)
 
+# First letter uppercase function
+firstup <- function(x) {
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  x
+}
+
 # Load Data
 initialcsv = as_tibble(read_csv(file="US Mass Shootings.csv"))
 
@@ -94,4 +100,88 @@ missing_location_completed$Location = list$Location
 ##
 
 
+##
+# START : Put Location in initial data
+##
+formattedCsv = initialcsv
+
+# Concatenate Location columns
+formattedCsv = formattedCsv %>% left_join(missing_location_completed, by= c("Longitude", "Latitude"))
+formattedCsv = formattedCsv %>% mutate_if(is.character, function(x) {replace_na(x, "")})
+formattedCsv = formattedCsv %>% unite(Location, Location.x, Location.y, sep="")
+
+# Concatenate Longitude and Latitude columns
+formattedCsv = formattedCsv %>% left_join(missing_lat_long_completed, by= "Location")
+formattedCsv = formattedCsv %>% mutate_if(is.numeric, function(x) {replace_na(x, 0)})
+formattedCsv = formattedCsv %>% mutate(Longitude.x = replace_na(Longitude.x, 0)) %>% mutate(Longitude.y = replace_na(Longitude.y, 0)) %>% mutate(Longitude = Longitude.x + Longitude.y)
+formattedCsv = formattedCsv %>% mutate(Latitude.x = replace_na(Latitude.x, 0)) %>% mutate(Latitude.y = replace_na(Latitude.y, 0)) %>% mutate(Latitude = Latitude.x + Latitude.y)
+# Deleting old columns
+formattedCsv = formattedCsv %>% select(-Latitude.x, -Latitude.y, -Longitude.x, -Longitude.y)
+##
+# END : Put Location in initial data
+##
+
+
+##
+# START : Separate Date to Year, Month, Day
+##
+formattedCsv = formattedCsv %>% mutate(Date = as.Date(Date, "%m/%d/%y"))
+
+formattedCsv$Day = weekdays(formattedCsv$Date)
+formattedCsv$Month = months(formattedCsv$Date)
+formattedCsv$Year = year(formattedCsv$Date)
+
+formattedCsv = formattedCsv %>% select(-Date)
+##
+# END : Separate Date to Year, Month, Day
+##
+
+
+##
+# START : Group by Gender, Race, Mental Health Issue, Open/Close Location, Cause, Target, Incident Area, Weapon Type
+##
+
+gByTribble = formattedCsv
+
+# Gender
+gByTribble = gByTribble %>% mutate(Gender = case_when(Gender == "Male" ~ "M", Gender == "Female" ~ "F", Gender == "Male/Female" ~ "M/F", Gender == "Unknown" ~ "M/F", TRUE ~ Gender))
+
+#Race
+gByTribble = gByTribble %>% mutate(Race = case_when(
+    grepl("[Ww]hite", Race) ~ "White", 
+    grepl("[Bb]lack", Race) ~ "Black",
+    grepl("[Aa]sian", Race) ~ "Asian",
+    grepl("[Nn]ative", Race) ~ "Native",
+    Race == "Some other race" | Race == "Two or more races" | Race == "Other" | Race == "" ~ "Unknown",
+    TRUE ~ Race
+  )
+)
+
+#Mental Health Issue
+gByTribble = gByTribble %>% mutate(`Mental Health Issues` = case_when(`Mental Health Issues` == "Unclear" ~ "Unknown", TRUE ~ `Mental Health Issues`))
+
+#Open/Close Location
+gByTribble = gByTribble %>% mutate(`Open/Close Location` = case_when(`Open/Close Location` == "Open+Close" ~ "Unknown", TRUE ~ `Open/Close Location`))
+
+#Cause
+gByTribble = gByTribble %>% mutate(Cause = case_when(Cause == "" ~ "Unknown", TRUE ~ firstup(Cause)))
+
+
+#Target
+gByTribble = gByTribble %>% mutate(Target = case_when(Target == "" ~ "Random", TRUE ~ firstup(Target)))
+
+
+#Incident Area
+gByTribble = gByTribble %>% mutate(`Incident Area` = case_when(`Incident Area` == "" ~ "Random", TRUE ~ firstup(`Incident Area`)))
+
+
+#Weapon Type
+gByTribble = gByTribble %>% mutate(`Weapon Type` = case_when(`Weapon Type` == "" ~ "Random", TRUE ~ firstup(`Weapon Type`)))
+
+
+
+
+##
+# END : Group by Gender, Race, Mental Health Issue, Open/Close Location, Cause, Target, Incident Area, Weapon Type
+##
 
